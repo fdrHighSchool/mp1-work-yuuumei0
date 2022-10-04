15bcc9e1928ecebec3305a423ca64eeb14520ed3
yuuumei0 <ylin14@schools.nyc.gov>
2022-10-04 14:09:03 -0400

    added fav movie file

---
+++ 01-favorite-movie/FavoriteMovie.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of class FavoriteMovie here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class FavoriteMovie
+{ public static void main(String[]args){
+    line1();
+    line5();
+    line6();
+    line5();
+    line4();
+    line1();
+    
+ }
+  public static void line1(){
+      System.out.println("   /\\   ");
+      System.out.println("  /  \\   ");
+      System.out.println(" /    \\   ");
+  
+  
+  }
+  public static void line4(){
+      System.out.println("+------+");
+  }
+  public static void line5(){
+      System.out.println("+------+");
+      System.out.println("|      |");
+      System.out.println("|      |");
+  }
+  public static void line6(){
+      System.out.println("+------+");
+      System.out.println("|UNITED|");
+      System.out.println("|STATES|");
+  }
+}
